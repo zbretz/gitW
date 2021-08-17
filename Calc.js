dd65d9febe0ea0ec6be4c4691e89363c134328ba
Zach <zachary.bretz@gmail.com>
2021-08-16 18:39:24 -0700

    Implemented add function

--- Calc.js
+++ Calc.js
@@ -1,7 +1,12 @@
 function Calculator(){
 
-  this.add = function(){}
+  this.add = function(a, b){
+    return a + b
+  }
 
   this.subtract = function(){}
 
 }
+
+var zachCalc = new Calculator()
+console.log(zachCalc.add(2,5))
